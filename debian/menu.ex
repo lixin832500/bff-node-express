15504f6e11a5fe9bd471b643fe242a76378fe8c7
Wilson Ding <hello@wilsonding.com>
2018-05-21 14:11:37 -0500

    Cleaned up unneccessary files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(test-0.0):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="test-0.0" command="/usr/bin/test-0.0"
--- debian/test-0.0.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the test-0.0 package
-#
-0 4	* * *	root	[ -x /usr/bin/test-0.0_maintenance ] && /usr/bin/test-0.0_maintenance
